99062dcf47c8c29db472e7dad6aed2a6310b7252
VincentB <vincent.brunelle.gm@gmail.com>
2025-09-23 23:34:17 -0400

    ADD : Game class

--- crossword-solver/Program.cs
+++ crossword-solver/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Extractor
+﻿using System.Runtime.InteropServices;
+
+public class Extractor
 {
     public string[] getFileContent(string filename)
     {
@@ -12,9 +14,15 @@
     }
 }
 
+public class Cell
+{
+    public char Letter { get; set; }
+    public bool Used { get; set; } = false;
+}
+
 public class Grid
 {
-    public char[,] Cells { get; set; }
+    public Cell[,] Cells { get; set; }
     public int GridWidth { get; set; }
     public int GridHeight { get; set; }
 
@@ -37,7 +45,7 @@ public class Grid
     {
         this.GridWidth = getGridWidth(data);
         this.GridHeight = getGridHeight(data);
-        this.Cells = new char[GridHeight, GridWidth];
+        this.Cells = new Cell[GridHeight, GridWidth];
         setGridLines(data);
     }
 
@@ -49,20 +57,62 @@ public class Grid
                 break;
             char[] lineToArray = data[i].ToCharArray();
             for (int c = 0; c < lineToArray.Length; c++)
-                Cells[i, c] = lineToArray[c];
+            {
+                Cell cell = new Cell();
+                cell.Letter = lineToArray[c];
+                Cells[i, c] = cell;
+            }
         }
     }
 
     public void printGrid()
     {
+        Console.WriteLine("Grid\n\n");
         for (int i = 0; i < GridHeight; i++)
         {
             for (int j = 0; j < GridWidth; j++)
             {
-                Console.Write(Cells[i, j]);
+                Console.Write(Cells[i, j].Letter);
             }
             Console.WriteLine("");
         }
+    }
+}
+
+public class Game
+{
+    public Grid Grid { get; set; }
+    public List<string> ListOfWords { get; set; }
+    public string Solution { get; set; }
+
+    public List<string> setListOfWords(string[] data)
+    {
+        List<string> listOfWords = new List<string>();
+        bool isWord = false;
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (isWord)
+                listOfWords.Add(data[i]);
+            if (data[i] == "")
+                isWord = true;
+        }
+        return listOfWords;
+    }
+
+    public void printGame()
+    {
+        Grid.printGrid();
+        Console.WriteLine("\n\nList of words\n\n");
+        foreach(string s in ListOfWords)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    public Game(string[] data)
+    {
+        this.Grid = new Grid(data);
+        this.ListOfWords = setListOfWords(data);
     }
 }
 
@@ -73,9 +123,7 @@ class Program
         Extractor extractor = new Extractor();
         string[] data = extractor.getFileContent("filename.txt");
 
-        Grid grid = new Grid(data);
-        Console.WriteLine($"Grid width = {grid.GridWidth}");
-        Console.WriteLine($"Grid height = {grid.GridHeight}");
-        grid.printGrid();
+        Game game = new Game(data);
+        game.printGame();
     }
 }
